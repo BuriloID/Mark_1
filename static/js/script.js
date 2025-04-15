41f20e0694545d6859cb756240ba5fbd0e5d4880
Danila Buryakov <danila.buryakovv@mail.ru>
2025-04-15 22:10:55 +0300

    new file:   __pycache__/app.cpython-313.pyc 	modified:   static/css/main.css 	new file:   static/img_catalog/product10.jpg 	new file:   static/img_catalog/product10_back.jpg 	new file:   static/img_catalog/product11.jpg 	new file:   static/img_catalog/product11_back.jpg 	new file:   static/img_catalog/product9.jpg 	new file:   static/img_catalog/product9_back.jpg 	new file:   static/img_product/product10_1.jpg 	new file:   static/img_product/product10_2.jpg 	new file:   static/img_product/product10_3.jpg 	new file:   static/img_product/product10_4.jpg 	new file:   static/img_product/product10_5.jpg 	new file:   static/img_product/product11_1.jpg 	new file:   static/img_product/product11_2.jpg 	new file:   static/img_product/product11_3.jpg 	new file:   static/img_product/product11_4.jpg 	new file:   static/img_product/product1_1.jpg 	new file:   static/img_product/product1_2.jpg 	new file:   static/img_product/product1_3.jpg 	new file:   static/img_product/product1_4.jpg 	new file:   static/img_product/product1_5.jpg 	new file:   static/img_product/product2_1.jpg 	new file:   static/img_product/product2_2.jpg 	new file:   static/img_product/product2_3.jpg 	new file:   static/img_product/product2_4.jpg 	new file:   static/img_product/product3_1.jpg 	new file:   static/img_product/product3_2.jpg 	new file:   static/img_product/product3_3.jpg 	new file:   static/img_product/product3_4.jpg 	new file:   static/img_product/product3_5.jpg 	new file:   static/img_product/product4_1.jpg 	new file:   static/img_product/product4_2.jpg 	new file:   static/img_product/product4_3.jpg 	new file:   static/img_product/product4_4.jpg 	new file:   static/img_product/product4_5.jpg 	new file:   static/img_product/product5_1.jpg 	new file:   static/img_product/product5_2.jpg 	new file:   static/img_product/product5_3.jpg 	new file:   static/img_product/product5_4.jpg 	new file:   static/img_product/product5_5.jpg 	new file:   static/img_product/product5_6.jpg 	new file:   static/img_product/product6_1.jpg 	new file:   static/img_product/product6_2.jpg 	new file:   static/img_product/product6_3.jpg 	new file:   static/img_product/product6_4.jpg 	new file:   static/img_product/product7_1.jpg 	new file:   static/img_product/product7_2.jpg 	new file:   static/img_product/product7_3.jpg 	new file:   static/img_product/product7_4.jpg 	new file:   static/img_product/product7_5.jpg 	new file:   static/img_product/product7_6.jpg 	new file:   static/img_product/product8_1.jpg 	new file:   static/img_product/product8_2.jpg 	new file:   static/img_product/product8_3.jpg 	new file:   static/img_product/product8_4.jpg 	new file:   static/img_product/product8_5.jpg 	new file:   static/img_product/product9_1.jpg 	new file:   static/img_product/product9_2.jpg 	new file:   static/img_product/product9_3.jpg 	new file:   static/img_product/product9_4.jpg 	new file:   static/img_product/product9_5.jpg 	modified:   static/js/script.js 	new file:   static/new_product/product1.jpg 	new file:   static/new_product/product1_1.jpg 	new file:   static/new_product/product1_2.jpg 	new file:   static/new_product/product1_3.jpg 	new file:   static/new_product/product1_4.jpg 	new file:   static/new_product/product1_5.jpg 	new file:   static/new_product/product1_back.jpg 	modified:   templates/product_detail.html

--- static/js/script.js
+++ static/js/script.js
@@ -50,3 +50,13 @@ function changeMainImage(newSrc) {
         }, 200); // Время должно совпадать с CSS-анимацией
     }
 }
+const openPopUp = document.getElementById('open_pop_up');
+const closePopUp = document.getElementById('pop_up_close');
+const popUp = document.getElementById('pop_up');
+openPopUp.addEventListener('click', function(e){
+    e.preventDefault();
+    popUp.classList.add('active');
+})
+closePopUp.addEventListener('click', () => {
+    popUp.classList.remove('active');
+})
