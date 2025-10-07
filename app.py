33eb68e056a5fafa769bd80e4784db7692861936
Danila Buryakov <danila.buryakovv@mail.ru>
2025-10-08 00:51:38 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -472,4 +472,4 @@ if __name__ == "__main__":
         db.create_all()
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=True)
